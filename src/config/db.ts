36760a09e5a045c0ac2385ff4649f20d2242cfbf
michael <michaelsanchez8815@gmail.com>
2022-07-20 22:19:13 -0500

    TS to JS files

--- src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI as string); 
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
-};
-
-export default connectDB;
