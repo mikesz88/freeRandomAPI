3a321a36c04931af17a6627873727e7f65e23e77
michael <michaelsanchez8815@gmail.com>
2022-07-21 21:55:20 -0500

    Update security protocols

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const path = require('path');
 dotenv.config({ path: 'config/config.env' });
@@ -24,6 +30,19 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 };
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(hpp());
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 500, // Limit each IP to 500 request per 'window' (here, per 15 minutes)
+});
+
+app.use(limiter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
