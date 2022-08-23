65b99f8069249aef421191630f3b36095a9edcb5
michael <michaelsanchez8815@gmail.com>
2022-08-22 22:01:45 -0500

    resolve comments

--- routes/desserts.js
+++ routes/desserts.js
@@ -2,13 +2,11 @@ const { Router } = require("express");
 const filteredResults = require('../middleware/filteredResults');
 const Dessert = require('../models/dessert');
 
-
 const router = Router();
 
 const { getDesserts, getDessertType } = require("../controller/dessert");
 
 router.get('/', filteredResults(Dessert), getDesserts);
 router.get('/:category', filteredResults(Dessert), getDessertType);
-
 
 module.exports = router;
