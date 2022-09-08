c7d33dd3c4ad54655718a07ca3b0e73014331dbe
michael <michaelsanchez8815@gmail.com>
2022-09-08 14:58:57 -0500

    adjust description in modal and readded data

--- models/dessert.js
+++ models/dessert.js
@@ -9,26 +9,29 @@ const DessertSchema = new mongoose.Schema({
   category: {
     type: String,
     enum: ['Cookie', 'Donut', 'Ice_Cream'],
-    required: [true, 'What type of dessert is it?']
+    required: [true, 'What type of dessert is it?'],
   },
   photoUrl: {
     type: String,
     required: [true, 'Where is the photo link?'],
     unique: true,
     match: [
-      /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig,
-      'Please enter a valid url'
+      /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
+      'Please enter a valid url',
     ],
   },
   photoAttribute: {
     type: String,
-    default: "This is public domain image",
+    default: 'This is public domain image',
+  },
+  description: {
+    type: String,
+    required: [true, 'Must have a description.'],
   },
   createdAt: {
     type: Date,
-    default: Date.now()
-  }
+    default: Date.now(),
+  },
 });
 
 module.exports = mongoose.model('Dessert', DessertSchema);
-
